afba3d0cfc3ef8fdcc5764b185ed8f456a095a29
ValeriGolovina <golovina.ler1@gmail.com>
2017-04-14 17:53:48 +0300

    Custom functions and arrays

--- index.php
+++ index.php
@@ -3,6 +3,31 @@
 $a=5;
 //var_dump($a);
 //die('break');
-echo 'Hello PHP!';
+
 define('Pi' , 3.14  );
 //print Pi;
+function hello($name = 'Friend')
+{
+    $message= "Hello" .$name."!<br>";
+    if ($name === 'Friend') {
+        $message = $message.'<a href ="">Login </a>';
+        $message .= '<a href ="">Login </a>';
+    }  elseif ($name === 'Lera') {$message = 'Yo man';}
+    else {$message .= '<a href ="">Logout </a>';}
+    return $message;
+}
+;
+$message = hello('Lera');
+print $message;
+
+$arr = [1, 2, 3];
+print $arr[1];
+$arr[4]=[1,2,44];
+print $arr[4][2];
+//ассоциативный массив
+$langs =[
+        'backend' =>['PHP', 'Pyton'],
+        'frontend' => ['HTML'],
+];
+print $langs['backend'][1];
+unset ($arr[1]);
