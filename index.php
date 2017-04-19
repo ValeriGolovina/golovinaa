45c54e383acdbde5242164f5d360c41d045276ff
ValeriGolovina <golovina.ler1@gmail.com>
2017-04-19 17:37:58 +0300

    Суперглобальные переменные

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ function hello($name = 'Friend')
 ;
 $message = hello('Lera');
 print $message;
-
+//Lesson3
 $arr = [1, 2, 3];
 print $arr[1];
 $arr[4]=[1,2,44];
@@ -29,5 +29,26 @@ $langs =[
         'backend' =>['PHP', 'Pyton'],
         'frontend' => ['HTML'],
 ];
-print $langs['backend'][1];
-unset ($arr[1]);
+//print $langs['backend'][1];
+
+foreach($langs as $key=>$value){
+   print '<h2>'.$key.'</h2>';
+   foreach ($value as $lang){
+       print '<div>'.$lang.'</div>';
+
+   }
+}
+$i=0;
+while ($i<10) {
+    print '<div>'.$i.'</div>';
+    $i++;
+}
+
+do {echo 'Hi!'; break;} while(false);
+
+//Lesson 4
+//суперглобальные переменные _$GET
+var_dump ($_GET);
+var_dump ($_POST);
+var_dump ($_SERVER);
+
